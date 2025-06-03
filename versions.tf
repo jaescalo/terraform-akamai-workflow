6780c03e6f3d73919fd182b39cedc65386ee20fc
Jaime Escalona <jaescalo@akamai.com>
2025-06-03 15:05:30 -0600

    fixed tf version required

--- versions.tf
+++ versions.tf
@@ -2,7 +2,7 @@ terraform {
   required_providers {
     akamai = {
       source  = "akamai/akamai"
-      version = "= ~>8.0"
+      version = "~> 8.0"
     }
     linode = {
       source  = "linode/linode"
