d06aaf0edd5a3653458debf4c7522a69f3e567ac
Jaime Escalona <jaescalo@akamai.com>
2025-06-05 10:48:14 -0600

    support for latest versions

--- versions.tf
+++ versions.tf
@@ -2,14 +2,14 @@ terraform {
   required_providers {
     akamai = {
       source  = "akamai/akamai"
-      version = "~> 8.0"
+      version = "~>8.0"
     }
     linode = {
       source  = "linode/linode"
       version = "= 2.23.0"
     }
   }
-  required_version = "~> 1.9"
+  required_version = "~> 1.12"
   # Linode S3 is our remote backend and we'll pass the configuration parameters with 
   # the TF "-backend-config" flag as this block doesn't allow the use of variables. 
   # See the ./github/workflows/akamai_pm.yaml.
